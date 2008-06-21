16104757370ca3d723d76d5d35e1466acff33260
Stefan Rusterholz <apeiros@gmx.net>
2008-06-21 18:14:13 +0200

    forgot some pieces in previous commits

--- trunk/rake/lib/project.rb
+++
@@ -1,8 +0,0 @@
-require 'ostruct'
-
-class Project < OpenStruct
-	class <<self
-		(private_instance_methods-%w(initialize undef_method inspect)).each { |m| undef_method m }
-		#undef_method "gem"
-	end
-end
--- trunk/rake/tasks/test.rake
+++
@@ -1,4 +0,0 @@
-import 'rake/tasks/spec.rake'
-
-desc 'Test is done by spec'
-task :test => 'spec:run'
