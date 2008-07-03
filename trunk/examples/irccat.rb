d22fe1fc11c57d5aba26d12c757833211d778484
Stefan Rusterholz <apeiros@gmx.net>
2008-07-03 22:02:12 +0200

    Fixed and improved irccat (thanks to erikh)

--- trunk/examples/irccat.rb
+++ trunk/examples/irccat.rb
@@ -8,6 +8,22 @@ $LOAD_PATH.unshift libdir if File.exist?(libdir)
 begin; require 'rubygems'; rescue LoadError; end
 require 'silverplatter/irc/connection'
 
+Usage = "Usage: irccat --server irc.example.com [options] [server] #channel ... [file ...]"
+
+# Provide usage if necessary
+if ARGV.empty? || !ARGV.grep(/^(?:(?:--)?help|-h|(?:--)?usage)$/).empty? then
+	abort(<<-EOH)
+irccat help:
+    #{Usage}
+--server:     server to connect to
+--port:       port to use
+--serverpass: password to enter the server
+--nick:       nickname to use (default: irccat)
+--user:       username to use (default: irccat)
+--real:       realname to use (default: silverplatter-irc-cat)
+	EOH
+end
+
 # extract opts
 def extract(opt)
 	return unless index = ARGV.index(opt)
@@ -16,40 +32,27 @@ def extract(opt)
 end
 
 BotConfig = {
-	:server          => extract '--server',
-	:port            => extract '--port' || 6667,
-	:serverpass      => extract '--serverpass',
+	:server          => extract('--server') || ARGV.shift
+	:port            => extract('--port') || 6667,
+	:serverpass      => extract('--serverpass'),
 	
 	:join            => [],
 
-	:nickname        => extract '--nick' || 'irccat',
-	:username        => extract '--user' || 'irccat',
-	:realname        => extract '--real' || 'silverplatter-irc-cat',
+	:nickname        => extract('--nick') || 'irccat',
+	:username        => extract('--user') || 'irccat',
+	:realname        => extract('--real') || 'silverplatter-irc-cat',
 	
 	:on_nick_error   => SilverPlatter::IRC::Connection::IncrementOnNickError,
 }
-BotConfig[:join] = ARGV.shift until File.exist?(ARGV.first)
+BotConfig[:join] << ARGV.shift until File.exist?(ARGV.first)
 
-if ARGV.include?('--help') || ARGV.include?('-h') then
-	abort(<<-EOH)
-irccat help:
-    Usage: irccat --server irc.example.com [options] #channel ... [file ...]
---server:     server to connect to
---port:       port to use
---serverpass: password to enter the server
---nick:       nickname to use (default: irccat)
---user:       username to use (default: irccat)
---real:       realname to use (default: silverplatter-irc-cat)
-	EOH
-elsif !BotConfig[:server] || ARGV.empty? then
-	abort("Usage: echo 'text' | irccat --server irc.freenode.org #channel ...")
-end
+abort(Usage) unless BotConfig[:server]
 
 Bot = SilverPlatter::IRC::Connection.new(nil, BotConfig)
 Bot.connect
 Bot.run
 Bot.login
 while line = gets
-	Bot.privmsg(line.chomp, *BotConfig[:join])
+	Bot.send_privmsg(line.chomp, *BotConfig[:join])
 end
 Bot.quit "My work here is done."
